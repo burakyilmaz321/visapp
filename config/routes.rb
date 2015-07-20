d1ebd9c103ff0f21a81b0f90d0d34b03d514a3be
burakyilmaz321 <burakyilmaz321@gmail.com>
2015-07-20 15:04:29 +0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
   get  'signup' => 'user_firm#new'
+  resources :user_firms
+  resources :user_firms do
+    resources :users
+  end
 end

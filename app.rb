2b948596ae610848404bc3ea371c7a75e4130147
burakyilmaz321 <burakyilmaz321@gmail.com>
2015-07-21 22:06:10 +0300

    Update app.rb

--- app.rb
+++ app.rb
@@ -1,1 +1,32 @@
+# UserFirm Model
+class UserFirm < ActiveRecord::Base
+  has_many :users
+  accepts_nested_attributes_for :users
+end
 
+# User Model
+class User < ActiveRecord::Base
+  belongs_to :user_firm
+end
+
+# Schema
+ActiveRecord::Schema.define do
+  create_table "", force: :cascade do |t|
+    
+  end
+end
+
+# user_firms controller
+class UserFirmsController < ApplicationController
+  
+end
+
+# users controller
+class UsersController < ApplicationController
+  
+end
+
+# Routes
+Rails.application.routes.draw do
+  
+end

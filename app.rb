58c32191552c2cc2879c1313659532aebf253d76
burakyilmaz321 <burakyilmaz321@gmail.com>
2015-07-21 22:12:48 +0300

    Update app.rb

--- app.rb
+++ app.rb
@@ -11,19 +11,52 @@ end
 
 # Schema
 ActiveRecord::Schema.define do
-  create_table "", force: :cascade do |t|
-    
+  create_table "user_firms", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+  
+    create_table "users", force: :cascade do |t|
+    t.integer  "user_firm_id"
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 end
 
 # user_firms controller
 class UserFirmsController < ApplicationController
+  def index
+  end
   
+  def show
+  end
+  
+  def new
+  end
+  
+  def create
+  end
+  
+  private
+    def user_firm_params
+    end
 end
 
 # users controller
 class UsersController < ApplicationController
+  def index
+  end
   
+  def show
+  end
+  
+  def new
+  end
+  
+  def create
+  end
 end
 
 # Routes
